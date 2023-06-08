bc5f3bd102d8ee0db2c99a710997ef689bac7d3c
Joe-Adobunu <ocheadozjay@gmail.com>
2023-06-08 19:43:26 +0100

    initial commit

---
+++ introduction/classworkTwo.js
@@ -0,0 +1,27 @@
+let x = 50;
+let y = 50;
+let n = x + y;
+let n1 = n * 2;
+
+let nn = 100;
+let n2 = nn % 10;
+let nn2 = nn + n2;
+
+let x1 = 50;
+let n_times = 2
+let xx = x1 * n_times;
+
+let a = 20;
+let productX = a + 10;
+let productY = a - 10;
+let productZ = a % 10;
+let productOf_x_y_z = productX * productY * productZ;
+let final_value = a / productOf_x_y_z;
+
+console.log(n1)
+console.log(nn2)
+console.log(xx)
+console.log(final_value)
+
+
+
---
+++ introduction/variables.js
@@ -0,0 +1,9 @@
+var number = 20;
+let age = 23;
+const SCHOOL = "Semicolon Africa";
+
+age = 25
+
+console.log(age);
+console.log(number);
+console.log(SCHOOL);
